6bbb6e0042eddbeb6d5c3e73c1f82903d1dbf7a9
JuanMa <juanma.garrido@gmail.com>
2018-05-30 19:58:36 +0200

    placholder

--- src/Image/index.js
+++ src/Image/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import cx from "classnames";
 import { compose } from "recompose";
 import { withLoadingState, withErrorState } from "./hoc";
-import { Error }  from "./components";
+import { Error } from "./components";
 
 import "./styles.css";
 
@@ -10,24 +10,33 @@ const BASE_CLASS = "react-Image";
 
 const Image = ({
   src,
+  placeholder,
   spinner: Spinner,
   handleLoad,
   handleError,
   loading,
-  error,
-  placeholderStyle
+  error
 }) => {
+  const placeholderStyle = {
+    backgroundImage: `url(${placeholder})`
+  };
 
   const classNames = cx(
-    BASE_CLASS, 
-    `is-${loading ? 'loading' : 'loaded'}`,
+    BASE_CLASS,
+    `is-${loading ? "loading" : "loaded"}`,
     error && `is-error`
   );
 
   // console.log({ src, loading, handleLoad, placeholderStyle });
   return (
     <figure style={placeholderStyle} className={classNames}>
-      <img alt="" src={src} className={`${BASE_CLASS}-image`} onLoad={handleLoad} onError={handleError} />
+      <img
+        alt=""
+        src={src}
+        className={`${BASE_CLASS}-image`}
+        onLoad={handleLoad}
+        onError={handleError}
+      />
       {loading && Spinner && <Spinner className={`${BASE_CLASS}-spinner`} />}
       {error && <Error />}
     </figure>
--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from "react";
 import { render } from "react-dom";
 
 import Spinner from "react-spinner";
-import 'react-spinner/react-spinner.css'
+import "react-spinner/react-spinner.css";
 
-import './styles.css'
+import "./styles.css";
 
 import Image from "./Image";
 
+const placeholderSrc = "https://picsum.photos/50?image=980";
 const imageSrc = "https://picsum.photos/5000?image=980";
 const imageSrcBad = "https://pics___um.photos/5000?image=980";
 
 const App = () => (
-  <div className='App'>
+  <div className="App">
+    <div className="imageContainer">
+      <Image
+        name="CodeSandbox"
+        spinner={Spinner}
+        placeholder={placeholderSrc}
+        src={imageSrc}
+      />
+    </div>
     <div className="imageContainer">
       <Image name="CodeSandbox" spinner={Spinner} src={imageSrc} />
     </div>
